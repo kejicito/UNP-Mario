d5ea8edd525085d3b533490ec6068336f448d06c
kejicito <contato.keji@gmail.com>
2025-03-17 20:54:33 -0300

    Add files via upload
    Lógicas de Programação - While

---
+++ Contadorwhile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contadorwhile;
+
+/**
+ *
+ * @author 12825137452
+ */
+public class Contadorwhile {
+
+    public static void main(String[] args) {
+        int contador = 10;
+        while(contador < 50) {
+            System.out.println("Repeticao nr: " + contador);
+            contador ++;
+    }
+}
+}
